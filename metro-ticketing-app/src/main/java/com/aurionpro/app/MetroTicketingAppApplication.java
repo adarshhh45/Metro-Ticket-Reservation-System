e8b10693f5767a6df0039e8a0f1c36784245ec7c
unknown <adarsh.prasad>
2025-09-18 14:22:15 +0530

    first commit

---
+++ metro-ticketing-app/src/main/java/com/aurionpro/app/MetroTicketingAppApplication.java
@@ -0,0 +1,13 @@
+package com.aurionpro.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroTicketingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroTicketingAppApplication.class, args);
+	}
+
+}
